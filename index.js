fa52bc80e19628e8dc60ad0e5e540c81d67ccfcf
nandasino <fernandamsino@gmail.com>
2022-11-07 10:25:32 -0300

    post sign-up

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const usuarios=[];
+
+app.post("/sign-up",(req,res)=>{
+    const {username, avatar} = req.body;
+    usuarios.push({
+        username,
+        avatar
+    });
+    console.log(usuarios);
+    res.send("OK");
+});
 
 app.listen(5000);
